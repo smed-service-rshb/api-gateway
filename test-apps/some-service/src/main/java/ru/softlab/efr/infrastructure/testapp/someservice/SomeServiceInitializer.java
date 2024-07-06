3e9123d4f55612a56c92ea0ec06e6b8d9dfae9b8
smed-service-rshb <SmedServiceRshb@yandex.ru>
2024-07-06 10:23:16 +0300

    Переход на Nexus [2]

--- test-apps/some-service/src/main/java/ru/softlab/efr/infrastructure/testapp/someservice/SomeServiceInitializer.java
+++
@@ -1,9 +0,0 @@
-package ru.softlab.efr.infrastructure.testapp.someservice;
-
-import ru.softlab.efr.infrastructure.transport.server.init.AbstractTransportInitializer;
-
-public class SomeServiceInitializer extends AbstractTransportInitializer {
-    protected Class<?> getConfiguration() {
-        return ApplicationConfiguration.class;
-    }
-}
